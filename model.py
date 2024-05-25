c0bf542f8775ca6dc6d38ec1952cea4b34da3add
Aldrin Estrada <130287432+alch1ve@users.noreply.github.com>
2024-05-25 15:45:43 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+def create_model(input_shape, num_classes):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
