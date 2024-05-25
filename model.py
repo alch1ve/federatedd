a4c55398c2e25e3f1ed88e5930549288e96711c6
alch1ve <aldrinestrada56@gmail.com>
2024-05-26 00:00:10 +0800

    model

--- model.py
+++ model.py
@@ -1,15 +1,11 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 def create_model(input_shape, num_classes):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(512, activation='relu', input_shape=(input_shape,)),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(256, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dense(num_classes, activation='softmax')
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(input_shape,)),
+        Dense(64, activation='relu'),
+        Dense(num_classes, activation='softmax')
     ])
     return model
